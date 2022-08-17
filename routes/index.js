f617834223cdf71d72a5924385b07c25cdb41c50
khansadwiputra <kansadwiputra@gmail.com>
2022-08-17 14:22:02 +0700

    nambah route dan folder controller

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,28 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// router.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+router.get('/', )
+router.get('/auth/register', )
+router.post('/auth/register', )
+router.get('/auth/login', )
+router.post('/auth/register', )
+
+router.get('/course', ) //show all courses (??)
+router.get('/course/:courseId', ) //show courses detail (?)
+router.get('/course/:courseId/edit', ) //edit course (only admin??)
+router.get('/course/:courseId/delete', )
+
+router.get('/auth/login', )
+router.get('/auth/login', )
+router.post('/auth/login', )
+
+
+
+
+
 
 module.exports = router
